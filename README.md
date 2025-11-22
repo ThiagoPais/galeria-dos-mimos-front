23689a071528d1536e118a7ae7635df5a4787564
Thiago Cardoso <thiago.card.pais@gmail.com>
2025-11-22 00:09:12 -0300

    Chore: Adding header

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# GaleriaDosMimosFront
+# GaleriaDeMimosFront
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.0.3.
 
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, galeria-dos-mimos-front');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, galeria-de-mimos-front');
   });
 });
